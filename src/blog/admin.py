2b7294009cf793dcb557eccdbb07da409a7f8be7
Diaby Almamy <diabyissouf149@gmail.com>
2022-08-07 02:38:24 +0000

    Model BlogPost revu et ajouté dans l'interface d'admin

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogPost
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "sub_title",
+        "published",
+        "author",
+        "last_edit",
+        "publish_date",
+    )
+
--- src/blog/models.py
+++ src/blog/models.py
@@ -4,14 +4,17 @@ from django.utils.text import slugify
 
 
 class BlogPost(models.Model):
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Auteur")
     slug = models.SlugField(max_length=255, blank=True, unique=True)
     title = models.CharField(max_length=255, unique=True, verbose_name="Titre")
     sub_title = models.CharField(max_length=255, blank=True, verbose_name="Sous titre")
     content = models.TextField(verbose_name="Contenu")
-    published = models.BooleanField(default=True)
-    last_edit = models.DateTimeField(auto_now=True)
-    publish_date = models.DateField(blank=True, null=True)
+    published = models.BooleanField(default=True, verbose_name="Publié")
+    last_edit = models.DateTimeField(auto_now=True, verbose_name="Dernière modif")
+    publish_date = models.DateField(blank=True, null=True, verbose_name="Publié le")
+
+    class Meta:
+        verbose_name = "Post"
 
     def __str__(self):
         return self.title
